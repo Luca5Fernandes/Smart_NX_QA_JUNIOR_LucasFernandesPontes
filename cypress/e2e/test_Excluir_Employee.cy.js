c88ed50d1e8c1e5e1b302b798d41c2593ff52b8f
Lucas Fernandes <lucasfernandespontes122@gmail.com>
2025-06-01 09:44:42 -0300

    Adição de Testes de Exclução de Cadastro

--- cypress/e2e/test_Excluir_Employee.cy.js
+++ cypress/e2e/test_Excluir_Employee.cy.js
@@ -13,15 +13,28 @@ describe('Testes de Cadastro', () => {
     cy.get('.oxd-main-menu-search').type('PIM');
     cy.get('.oxd-main-menu-item > .oxd-text').click();
     cy.get('.orangehrm-header-container > .oxd-button').click()
-    cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').type('Lucas')
-    cy.get(':nth-child(2) > :nth-child(2) > .oxd-input').type('Fernandes')
-    cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').type('Pontes')
-    cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').clear('')
-    cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').type('0416')
+    cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').type('Teste')
+    cy.get(':nth-child(2) > :nth-child(2) > .oxd-input').type('Cancelar')
+    cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').type('Exclusão')
+    cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').clear('').type('9999')
     cy.get('.oxd-button--secondary').click()
+    cy.wait(5000);
   });
-  it('Não Exclusão', () => {
-
-    
-});
+  it('Cancelar Exclusão de Cadastro', () => {
+    cy.get(':nth-child(2) > .oxd-main-menu-item').click()
+    cy.get(':nth-child(2) > .oxd-input').type('9999')
+    cy.wait(2000);
+    cy.get('.oxd-form-actions > .oxd-button--secondary').click()
+    cy.wait(4000);
+    cy.get('.oxd-table-cell-actions > :nth-child(2)').click()
+    cy.get('.orangehrm-modal-footer > .oxd-button--ghost').click()    
+  });
+   it('Exclusão de Cadastro', () => {
+    cy.get(':nth-child(2) > .oxd-main-menu-item').click()
+    cy.get(':nth-child(2) > .oxd-input').type('9999')
+    cy.get('.oxd-form-actions > .oxd-button--secondary').click()
+    cy.wait(2000);
+    cy.get('.oxd-table-cell-actions > :nth-child(2)').click()
+    cy.get('.oxd-button--label-danger').click()
+  });
 });
